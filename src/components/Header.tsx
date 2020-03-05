e8031fc5c3005b334fb7bb017435fffa7ce380d4
MTLY-1610 <larrouy.m@me.com>
2020-03-05 11:14:20 +0100

    alt on header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ export default class Header extends React.Component<Props, State> {
   render() {
     return (
       <div className="header-div">
-        <img src={window.location.origin + "/images/sun-logo.png"} />
+        <img src={window.location.origin + "/images/sun-logo.png"} alt="SUN" />
         <h2>MM Weather</h2>
       </div>
     );
