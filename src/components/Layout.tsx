9f58082d723f3932df02fd50d946136112d08ef1
Mattin <mattin-najafi@hotmail.com>
2020-03-05 09:49:53 +0100

    header and searchbar starter

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,16 +3,27 @@ import Header from "./Header";
 import Flexbox from "./Flexbox";
 import Searchbar from "./Searchbar";
 
+const API_KEY = "c4a99f7da984ba9ec8a4964085bcd87e";
+
 export interface LayoutProps {}
 
 export interface LayoutState {}
 
 class Layout extends React.Component<LayoutProps, LayoutState> {
+  getWeather = async (event: React.MouseEvent<HTMLElement, MouseEvent>) => {
+    const city = "London";
+    const API_CALL = await fetch(
+      `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
+    );
+    const data = await API_CALL.json();
+    console.log(data);
+  };
+
   render() {
     return (
       <div>
         <Header />
-        <Searchbar />
+        <Searchbar nameOfCity={this.getWeather} />
         <Flexbox />
       </div>
     );
--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -1,11 +1,42 @@
 import * as React from "react";
 
-export interface Props {}
+export interface Props {
+  nameOfCity: (event: React.MouseEvent<HTMLElement, MouseEvent>) => void;
+}
 
-export interface State {}
+export interface State {
+  city: string;
+}
 
 export default class Searchbar extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      city: ""
+    };
+  }
+
+  handleChangeOnSearchbar = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ city: event.target.value });
+    console.log(this.state.city);
+  };
+
   render() {
-    return <div>Searchbar</div>;
+    return (
+      <div className="searchbar-div">
+        <input
+          className="searchbar"
+          placeholder="Enter city here.."
+          type="text"
+          defaultValue={this.state.city}
+          onChange={this.handleChangeOnSearchbar}
+        />
+        <i
+          id="searchicon"
+          className="fas fa-search"
+          onClick={this.props.nameOfCity}
+        />
+      </div>
+    );
   }
 }
