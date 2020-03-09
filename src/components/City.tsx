5385e087b9804e7c134a02812b9de07cdb7eb9ae
Mattin <mattin-najafi@hotmail.com>
2020-03-09 14:43:16 +0100

    Changin css and headerofflexbox

--- src/components/City.tsx
+++ src/components/City.tsx
@@ -4,10 +4,27 @@ export interface Props {
   CityName: string;
 }
 
-export interface State {}
+export interface State {
+  weekday: string;
+  time: number;
+}
+
+let date = new Date();
 
 export default class City extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      weekday: "",
+      time: 0
+    };
+  }
+
   render() {
-    return <div>{this.props.CityName}</div>;
+    return (
+      <div className="header-flexbox">
+        <h3>{this.props.CityName}</h3>
+      </div>
+    );
   }
 }
