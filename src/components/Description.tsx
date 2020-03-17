51d337d41239e3dd79866cc88d33b322c81557be
Mattin <mattin-najafi@hotmail.com>
2020-03-17 16:45:19 +0100

    fixing homepage

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -49,7 +49,7 @@ class Description extends React.Component<DescriptionProps, DescriptionState> {
   };
   render() {
     if (this.props.temperature === undefined) return null;
-    return <div className="description">"{this.tempDescription()}"</div>;
+    return <div className="description">{this.tempDescription()}</div>;
   }
 }
 
